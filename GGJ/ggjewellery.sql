13ee0665c08048824305f2923a73e06b851e9690
FRENCH Jake <jfrench@europe.corp.altran.com>
2018-03-16 10:18:47 +0100

    Added db sql

---
+++ GGJ/ggjewellery.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 16, 2018 at 09:17 AM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ggjewellery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gemstones`
+--
+
+DROP TABLE IF EXISTS `gemstones`;
+CREATE TABLE IF NOT EXISTS `gemstones` (
+  `Gem_Id` int(11) NOT NULL,
+  `Gem_Name` int(11) NOT NULL,
+  `Gem_Zodiac` int(11) NOT NULL,
+  `Gem_Image` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
